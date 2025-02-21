d4ebca9fe514a8e0007689879a349ef6a55eabde
isikdev <isikjandev@gmail.com>
2025-02-21 19:06:43 +0500

    101%

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'pwa-demo-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
+    'https://isikdev.github.io/pwa-demo/',
+    'https://isikdev.github.io/pwa-demo/index.html',
+    'https://isikdev.github.io/pwa-demo/manifest.json',
     'https://cdn.pixabay.com/photo/2017/09/23/16/33/pixel-heart-2779422_1280.png'
 ];
 
